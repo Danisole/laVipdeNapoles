454461aecf24f26e45ccefb3fa16ee50213cfd9d
danisole <danisole82@gmail.com>
2022-03-07 01:45:07 -0300

    agregando js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fulImgBox = document.getElementById("fulImgBox");
 const fulImg = document.getElementById("fulImg");
+const ham = document.querySelector(".ham");
+const enlaces = document.querySelector(".containLi");
+const barras = document.querySelectorAll(".ham span");
 
+/*fotos*/
 function openFulImg(reference) {
   fulImgBox.style.display = "flex";
   fulImg.src = reference;
@@ -9,3 +13,25 @@ function openFulImg(reference) {
 function closeImg() {
   fulImgBox.style.display = "none";
 }
+
+/*menu hamburguesa*/
+
+ham.addEventListener("click", () => {
+  enlaces.classList.toggle("activado");
+  barras.forEach(child => {child.classList.toggle("animado")});
+})
+
+
+/*iconos redes*/
+
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".contentRedes");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight/1;
+  if(contentPosition < screenPosition){
+    content.classList.add("active");
+  }else{
+    content.classList.remove("active")
+  }
+
+});
